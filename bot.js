4c303e0c1cf34aac5973186468474d9d0d2aeb4b
alik2121 <88913725+alik2121@users.noreply.github.com>
2021-12-02 01:55:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ instagram : adimas_shadoet
 */
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf('1675749539:AAH46KNMRschhJDMuI5dclpp9FnB0znEaK4')
+const bot = new Telegraf('5048875884:AAHFj_eQCrR56G0CuhOGkNJmB5GyzRFNXuw')
 const axios = require('axios')
 
 /*********[ Apikey ]*********/
